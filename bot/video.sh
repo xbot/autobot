f18eddfd89a2719096e79d13e28285b9def6e12c
xbot <donie.leigh@gmail.com>
2015-02-01 20:46:15 +0800

    start mjpg_streamer by absolute path

--- bot/video.sh
+++ bot/video.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
-mjpg_streamer -i "input_uvc.so -y -n -r 320x240 -f 10" -o "output_http.so -w /usr/local/www"
+/usr/local/bin/mjpg_streamer -i "input_uvc.so -y -n -r 320x240 -f 10" -o "output_http.so -w /usr/local/www"
